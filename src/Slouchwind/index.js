af2bbe0ae05f2904fdc460fabba4f000d158f280
Slouchwind <2241051890@qq.com>
2022-07-05 15:16:18 +0800

    io-head

--- src/Slouchwind/index.js
+++ src/Slouchwind/index.js
@@ -5,16 +5,19 @@ import "./index.css";
 export class Head extends React.Component {
     render() {
         return (
-            <div className="head">
-                <div id="left">
-                    <Icon src="https://m.xiguacity.cn/post/6042be99fef5f8753d6c03c1/c3750ecf-94c1-4d3f-9f3a-4bf2834fd779.svg" />
-                    <LinkBlock href="./editor">项目</LinkBlock>
+            <>
+                <div className="head">
+                    <div id="left">
+                        <Icon src="https://m.xiguacity.cn/post/6042be99fef5f8753d6c03c1/c3750ecf-94c1-4d3f-9f3a-4bf2834fd779.svg" />
+                        <LinkBlock href="./editor">项目</LinkBlock>
+                    </div>
+                    <div className="right">
+                        <Clock />
+                        <ColorMode />
+                    </div>
                 </div>
-                <div className="right">
-                    <Clock />
-                    <ColorMode />
-                </div>
-            </div>
+                <div id="top"></div>
+            </>
         );
     }
 }
--- src/Slouchwind/root.js
+++ src/Slouchwind/root.js
@@ -3,5 +3,64 @@ import ReactDOM from "react-dom/client";
 import { Head } from "./"
 import "../index.css";
 
+const index =
+    <>
+        <Head />
+        <h3>清晨花蕊，静满泪水。</h3>
+        <h3>是起是坠，真情清脆。</h3>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+        <p>测试</p>
+    </>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Head />);
+root.render(index);
